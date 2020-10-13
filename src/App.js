4061456dfd7e0aedb57349c07fd1f370aa15b4d7
April Darger <april.m.darger@gmail.com>
2020-10-13 14:06:36 -0600

    imported useState hook and starting to set up and render team members.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TeamMate from './components/teamMate'
 
 function App() {
+
+  const [teamMembers, setTeamMembers] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      {teamMembers.map((teamMate) => {
+        return <TeamMate key={teamMate.id} details={teamMate} />
+      })}
     </div>
   );
 }
---
+++ src/components/teamMate.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TeamMate(props) {
+    const { details } = props
+
+    if (!details) {
+        return <h3>Waiting for &apos;s details...</h3>
+    }
+
+    return (
+        <div className='teamMate container'>
+            <h2>{details.name}</h2>
+            <p>Email: {details.email}</p>
+            <p>Role: {details.role}</p>
+        </div>
+    )
+}
+
+export default TeamMate;
